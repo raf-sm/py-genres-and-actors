2ae40189881108141208dde588433f2ef3c3a6ec
raf-sm <rafasmendonca007@gmail.com>
2026-06-18 17:11:16 -0300

    Solution

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
 
+from db.models import Actor, Genre
+
 
 def main() -> QuerySet:
-    western = Genre.objects.create(name="Western")
-    action = Genre.objects.create(name="Action")
-    dramma = Genre.objects.create(name="Dramma")
-    george_klooney = Actor.objects.create(first_name="George", last_name="Klooney")
-    kianu_reaves = Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    scarlet_keegan = Actor.objects.create(first_name="Scarlet", last_name="Keegan")
-    will_smith = Actor.objects.create(first_name="Will", last_name="Smith")
-    jaden_smith = Actor.objects.create(first_name="Jaden", last_name="Smith")
-    scarlett_johansson = Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+    Genre.objects.create(name="Western")
+    Genre.objects.create(name="Action")
+    Genre.objects.create(name="Dramma")
+    Actor.objects.create(first_name="George", last_name="Klooney")
+    Actor.objects.create(first_name="Kianu", last_name="Reaves")
+    Actor.objects.create(first_name="Scarlet", last_name="Keegan")
+    Actor.objects.create(first_name="Will", last_name="Smith")
+    Actor.objects.create(first_name="Jaden", last_name="Smith")
+    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
 
     genre = Genre.objects.get(name="Dramma")
     genre.name = "Drama"
@@ -22,7 +24,7 @@ def main() -> QuerySet:
     actor.save()
     actor = Actor.objects.get(first_name="Kianu")
     actor.first_name = "Keanu"
-    actor.save
+    actor.save()
     actor = Actor.objects.get(last_name="Reaves")
     actor.last_name = "Reeves"
     actor.save()
@@ -31,4 +33,3 @@ def main() -> QuerySet:
     Actor.objects.filter(first_name="Scarlett").delete()
 
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
-    actor.save()
